021400c9f19aa65c011c392ba6b8d3e406bc4115
Stanley Smith <stancsmith@gmail.com>
2016-02-19 15:40:18 -0500

    update expressapp.js

--- ExpressApp.js
+++ ExpressApp.js
@@ -4,19 +4,17 @@ var express = require('express');
 //calls in the application instance 
 var app = express();
 
-/*
 app.get('/', function(request, response) {
 	//response.send('Hello World!');
 	//both write & end are node functions
 	response.write('Hello World!');
 	response.end();
-}); 
-*/
+});
 
 app.get('/blocks', function(request, response) {
 	var blocks = ['Fixed','Movable','Rotating','banana'];
 	//response.send(blocks);
-	//response.json helps the response read better.
+	//response.json helps the response read better than send.
 	response.json(blocks);
 });
 
